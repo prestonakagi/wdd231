fe2e7da67d44e10664b9b1811324ad928a733439
prestonakagi <preston.akagi@gmail.com>
2025-05-08 23:44:33 -0600

    initialize files and starting content for each.

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+// footer year and datetime last modified
+
+// select the DOM element for year output
+const year = document.getElementById("currentyear");
+
+// use the date object
+const today = new Date();
+
+const currentYear = today.getFullYear();
+
+// change text displayed on rendered web page
+year.innerText = currentYear
+
+
+// select the DOM element for last modified output
+const lastMod = document.getElementById("lastModified");
+
+// use lastModified property of document
+// default format is mm/dd/yyyy hh:mm:ss
+lastMod.innerText = document.lastModified;
+
+
+// responsive hamburger effect on nav menu.
+
+// hamburger button only show in mobile (small) view.
+// clicking hamburger button toggles nav menu items from viewable to not viewable.
+// use a symbol, like "X", to close hamburger menu.
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
