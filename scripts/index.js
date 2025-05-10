0fd24d8afd063340a74464843854509a2c0e84a9
prestonakagi <preston.akagi@gmail.com>
2025-05-10 11:21:20 -0600

    added images, main content including JavaScript elements

--- scripts/index.js
+++ scripts/index.js
@@ -33,3 +33,129 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('show');
     hamButton.classList.toggle('show');
 });
+
+
+// header picture and name
+const myName = `Preston Akagi`;
+const profile = {
+    className: "logo",
+    path: "images/profile.webp", 
+    alternate: "Preston Akagi Profile Picture"
+}
+
+const banner = document.getElementById("pic-heading");
+// create child elements of img and p
+const picElement = document.createElement("img");
+picElement.setAttribute("class", profile.className);
+picElement.setAttribute("src", profile.path);
+picElement.setAttribute("alt", profile.alternate);
+const nameElement = document.createElement("p");
+nameElement.innerText = myName;
+
+banner.appendChild(picElement);
+banner.appendChild(nameElement);
+
+
+// Course Work content
+const classes =[];
+
+
+// Certificate courses array of objects of each subject
+const courses = [
+    {
+        subject: 'CSE',
+        number: 110,
+        title: 'Introduction to Programming',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        technology: [
+            'Python'
+        ],
+        completed: false
+    },
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+            'HTML',
+            'CSS'
+        ],
+        completed: false
+    },
+    {
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        technology: [
+            'Python'
+        ],
+        completed: false
+    },
+    {
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+        technology: [
+            'C#'
+        ],
+        completed: false
+    },
+    {
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    },
+    {
+        subject: 'WDD',
+        number: 231,
+        title: 'Frontend Web Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    }
+]
+
+// array with number of subjects in courses
+const numberOfSubjects = courses.length;
+const coursesIndexes = [];
+for (let i = 0; i < length; i++) {
+    coursesIndexes.push(i);
+}
+
+// display all courses
+const certificateDiv = document.querySelector('.classes-grid');
+courses.forEach(course => {
+    const certChild = document.createElement("p");
+    certChild.innerText = `${course.subject} ${course.number}`;
+    certificateDiv.appendChild(certChild);
+});
+
+// display filtered courses for WDD courses and CSE courses
+// using buttons that listen for the click event.
+
