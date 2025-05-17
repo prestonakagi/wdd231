5c528a3f7e35130b0564156cfc4943aee80e561c
prestonakagi <preston.akagi@gmail.com>
2025-05-17 00:40:51 -0600

    added hero images, logo image, added js business cards, started css large.

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -35,15 +35,55 @@ hamButton.addEventListener('click', () => {
 });
 
 
+const cards = document.querySelector('#members');
+
 async function getData() {
-const response = await fetch('URL from GitHub');
-const data = await response.json();
-console.table(data);
+    const response = await fetch('https://raw.githubusercontent.com/prestonakagi/wdd231/refs/heads/main/chamber/data/members.json');
+    const data = await response.json();
+    // console.table(data);
+    displayCards(data.companies); //references the array not just the overall (single) object.
 }
 
 // look at prophets API assignment for anything else in the getData() function.
 
 getData();
+
+const displayCards = (companies) => {
+    companies.forEach((company) => {
+    let card = document.createElement("section");
+    let name = document.createElement("h2");
+    let businessTagLine = document.createElement("p"); //need put some info in JSON
+    let address = document.createElement("p");
+    let email = document.createElement("p");
+    let phone = document.createElement("p");
+    let url = document.createElement("p");
+    let icon = document.createElement("img");
+
+    name.innerText = `${company.name}`;
+    businessTagLine.innerText = `temporary holder`;
+    address.innerText = `ADDRESS: ${company.address}`;
+    email.innerText = `EMAIL: ${company.email}`;
+    phone.innerText = `PHONE: ${company.phone}`;
+    url.innerText = `URL: ${company.website}`;
+
+    icon.setAttribute("src", company.icon);
+    icon.setAttribute("alt", `Icon of ${company.name}`);
+    icon.setAttribute("loading", "lazy");
+    //icon.setAttribute("width", "340");
+    //icon.setAttribute("height", "440");
+    
+    card.appendChild(name);
+    card.appendChild(businessTagLine);
+    card.appendChild(address);
+    card.appendChild(email);
+    card.appendChild(phone);
+    card.appendChild(url);
+    card.appendChild(icon);
+
+    cards.appendChild(card);
+    });
+}
+
 
 // chamber's contact info
 const chamberContact = {title: "Draper Chamber of Commerce", street: "123 E 45678 S", city: "Draper, Utah 84020", email: "info@drapercc.org", phone: "(801) 333-4444"}
