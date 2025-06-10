ae8a620c00a70ece2eddadf0564f21bbb45eb4c8
prestonakagi <preston.akagi@gmail.com>
2025-06-10 00:55:36 -0600

    added interests images. Need to do layout

--- chamber/data/interests.mjs
+++ chamber/data/interests.mjs
@@ -3,48 +3,48 @@ export const places = [
     name: "Loveland Living Planet Aquarium",
     address: "12033 S Lone Peak Pkwy, Draper, UT 84020",
     description: "A 136,000 ft² aquatic attraction showcasing global ecosystems—from Utah waterways to ocean habitats—with penguins, sharks, sloths, and interactive exhibits.",
-    photo_url: "images/facebook-icon.svg"
+    photo_url: "images/aquarium.webp"
   },
   {
     name: "Draper Utah Temple",
     address: "14065 Canyon Vista Ln, Draper, UT 84020",
     description: "A 57,000 ft² LDS temple on a 12-acre site, notable for its towering spire, Moroni statue, landscaped grounds, and mountain backdrop.",
-    photo_url: "images/facebook-icon.svg"
+    photo_url: "images/draper-temple.webp"
   },
   {
     name: "Andy Ballard Arena",
     address: "1600 E Highland Dr (13675 S), Draper, UT 84020",
     description: "Hosts equestrian events, rodeos, monster truck shows, and serves as a trailhead location during Draper events.",
-    photo_url: "images/facebook-icon.svg"
+    photo_url: "images/rodeo.webp"
   },
   {
     name: "Draper City Park",
     address: "12500 S 1300 E, Draper, UT 84020",
     description: "Large community park with two playgrounds, family picnic areas, shade, and green space.",
-    photo_url: "images/facebook-icon.svg"
+    photo_url: "images/park.webp"
   },
   {
     name: "Cowabunga Bay",
     address: "12047 S State St, Draper, UT 84020",
     description: "Family-friendly water park featuring slides, a lazy river, cabanas, and seasonal operation.",
-    photo_url: "images/facebook-icon.svg"
+    photo_url: "images/water-park.webp"
   },
   {
     name: "South Mountain Golf Course",
     address: "1247 E Mike Weir Dr, Draper, UT 84020",
     description: "Public 18-hole, par-72 course offering mountain views and challenging play.",
-    photo_url: "images/facebook-icon.svg"
+    photo_url: "images/golf.webp"
   },
   {
     name: "Wasatch Hang Gliding LLC",
     address: "Point of the Mountain Flight Park, Draper, UT 84020",
     description: "Offers hang-gliding instruction, tandem flights, gear sales & service.",
-    photo_url: "images/facebook-icon.svg"
+    photo_url: "images/hang-glider.webp"
   },
   {
     name: "TRAX Draper Town Center Station",
     address: "1131 E Pioneer Rd, Draper, UT 84020",
     description: "Terminal stop on UTA TRAX Blue Line, with parking, accessibility, and outdoor waiting areas.",
-    photo_url: "images/facebook-icon.svg"
+    photo_url: "images/train.webp"
   }
 ];
