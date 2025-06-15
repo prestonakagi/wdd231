d905070606a2e506a6b17819e35d809b6bbe7443
prestonakagi <preston.akagi@gmail.com>
2025-06-14 18:11:07 -0600

    made modal display and not display on clicks of the 2 buttons, so can do infinite times!

--- final/scripts/modal.mjs
+++ final/scripts/modal.mjs
@@ -6,20 +6,22 @@
 // need 1st arg for selected ID and 2nd arg = data.specific level.
 
 export function displayField(whichModal, imagePath) {
-//   whichModal is for selection of dialog element!
+// whichModal is for selection of dialog element!
   whichModal.innerHTML = `
     <button type="button" id="closeModal">X</button>
     <h2>Ultimate Frisbee Field of Play</h2>
     <img src="${imagePath}" alt="Field of Play Diagram" load="lazy">
   `;
   whichModal.showModal();
+  whichModal.classList.remove('inactive');
+  whichModal.classList.add('active');
 
   // To close the modal when click X button
     const closeButton = document.getElementById('closeModal');
 
   closeButton.addEventListener("click", () => {
-    whichModal.close();
-    // TODO: first modal can be closed, but 2nd modal opens but cannot be closed on click!
-    // Actually kind of random which modal cannot be closed.
+    whichModal.close(); // I believe this clears the darkened background.
+    whichModal.classList.remove('active');
+    whichModal.classList.add('inactive');
   });
 };
